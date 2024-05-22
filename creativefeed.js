33c3b556659cc675c65524bc630e1b0e9e6b2302
Julia Paranay <jeparanay@comcast.net>
2024-05-22 11:37:55 -0400

    trying to remove video with js

--- creativefeed.js
+++ creativefeed.js
@@ -1,32 +1,47 @@
 $(document).ready(function () {
-
   // Check window width
-  var breakpoint = 501;
-  
+  var breakpoint = 768;
+
   // Feed hover over image to show video if window width is greater than breakpoint
   if (window.innerWidth > breakpoint) {
-    $(".viewer").mouseenter(function () {
-      var $el = $(this);
-      $el.find(".thumb").hide();
-      $el.find("video").show()[0].play();
-    }).mouseleave(function () {
-      var $el = $(this);
-      $el.find(".thumb").show();
-      $el.find("video").hide()[0].pause();
-    });
+    $(".viewer")
+      .mouseenter(function () {
+        var $el = $(this);
+        $el.find(".thumb").hide();
+        $el.find("video").show()[0].play();
+      })
+      .mouseleave(function () {
+        var $el = $(this);
+        $el.find(".thumb").show();
+        $el.find("video").hide()[0].pause();
+      });
 
-    $(".imgviewer").mouseenter(function () {
-      var $el = $(this);
-      $el.find(".thumb").hide();
-      $el.find(".thumb2").show();
-    }).mouseleave(function () {
-      var $el = $(this);
-      $el.find(".thumb").show();
-      $el.find(".thumb2").hide();
-    });
+    $(".imgviewer")
+      .mouseenter(function () {
+        var $el = $(this);
+        $el.find(".thumb").hide();
+        $el.find(".thumb2").show();
+      })
+      .mouseleave(function () {
+        var $el = $(this);
+        $el.find(".thumb").show();
+        $el.find(".thumb2").hide();
+      });
   }
 
+  // Function to check if the device is a mobile device based on screen size
+  function isMobileDevice() {
+    return window.matchMedia("(max-width: 768px)").matches;
+  }
 
+  // Check if the device is a mobile device
+  if (isMobileDevice()) {
+    // Remove the video element if it's a mobile device
+    const videoContainer = document.querySelector("video");
+    if (videoContainer) {
+      videoContainer.parentNode.removeChild(videoContainer);
+    }
+  }
 
   // Get the text element
   const textElement = document.getElementById("disappear");
